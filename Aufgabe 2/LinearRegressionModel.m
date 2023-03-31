d70691b4f414d8775ce4a6efbfd51b33a07d2e61
Marlon Oberberger <oberberger.marlon@gmail.com>
2023-03-31 13:39:52 +0200

    Added Aufgabe 2

---
+++ Aufgabe 2/LinearRegressionModel.m
@@ -0,0 +1,149 @@
+classdef LinearRegressionModel < matlab.mixin.SetGet
+    %LINEARREGRESSIONMODEL 
+    % Class representing an implementation of linear regression model
+    
+    properties (Access = public)
+        optimizer
+        trainingData
+        theta
+        thetaOptimum
+    end
+    
+    methods (Access = public)
+        function obj = LinearRegressionModel(varargin)
+            %LINEARREGRESSIONMODEL Construct an instance of this class
+            
+            % ========= YOUR CODE HERE =========
+            % perform the varargin
+            for i = 1:nargin
+                if strcmp(varargin{i},'Data')
+                    obj.trainingData = varargin{i+1};
+                elseif strcmp(varargin{i},'Optimizer')
+                    obj.optimizer = varargin{i+1};
+                end
+            end
+            
+            obj.initializeTheta();
+        end
+        
+        function J = costFunction(obj)
+            m = obj.trainingData.numOfSamples; 
+            
+            % ========= YOUR CODE HERE =========
+            % compute the costs
+            % therefore use the hypothesis function as well
+            % this calculation can be done by one line of code
+            % returns the cost value J
+
+            J = sum((obj.hypothesis() - obj.trainingData.commandVar).^2) / (2 * m);
+            
+        end
+        
+        function hValue = hypothesis(obj)
+            X = [ones(obj.trainingData.numOfSamples,1) obj.trainingData.feature];
+            
+            % ========= YOUR CODE HERE =========
+            % compute the hypothesis values for each sample
+            % therefore compute the matrix multiplication with X
+            % this calculation can be done by one line of code
+
+            hValue = X * obj.theta;
+            
+        end
+        
+        function h = showOptimumInContour(obj)
+            h = figure('Name','Optimum');
+            theta0_vals = linspace(50, 150, 100);
+            theta1_vals = linspace(0, 2, 100);
+            
+            % ========= YOUR CODE HERE =========
+            % compute the costs for each theta_vals tuple
+            [theta0_grid, theta1_grid] = meshgrid(theta0_vals, theta1_vals);
+            costVals = zeros(length(theta0_vals), length(theta1_vals));
+            for i = 1:length(theta0_vals)
+                for j = 1:length(theta1_vals)
+                    obj.setTheta(theta0_vals(i), theta1_vals(j));
+                    costVals(i, j) = obj.costFunction();
+                end
+            end
+            
+            
+            % plot the costs with the contour command
+            contour(theta0_grid, theta1_grid, costVals,20);
+
+            % add x and y label
+            xlabel('\theta_0');
+            ylabel('\theta_1');
+            hold on;
+            % add the optimum theta value to the plot (red X, MarkerSize: 10, LineWidth: 2)
+            plot(obj.thetaOptimum(1), obj.thetaOptimum(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+            hold off;
+            
+        end
+        
+        function h = showCostFunctionArea(obj)
+            h = figure('Name','Cost Function Area');
+            theta0_vals = linspace(50, 150, 100);
+            theta1_vals = linspace(0, 2, 100);
+            
+            % ========= YOUR CODE HERE =========
+            % compute the costs for each theta_vals tuple
+            [theta0Grid, theta1Grid] = meshgrid(theta0_vals, theta1_vals);
+            costVals = zeros(length(theta0_vals), length(theta1_vals));
+            for i = 1:length(theta0_vals)
+                for j = 1:length(theta1_vals)
+                    obj.setTheta(theta0_vals(i), theta1_vals(j));
+                    costVals(i, j) = obj.costFunction();
+                end
+            end
+            
+        
+            % plot the costs with the surf command
+            surf(theta0Grid, theta1Grid, costVals);
+            % add x and y label
+            xlabel('\theta_0');
+            ylabel('\theta_1');
+
+        end
+        
+        function h = showTrainingData(obj)
+           h = figure('Name','Linear Regression Model');
+           plot(obj.trainingData.feature,obj.trainingData.commandVar,'rx')
+           grid on;
+           xlabel(obj.trainingData.featureName + " in Kelvin");
+           ylabel(obj.trainingData.commandVarName + " in Kelvin");
+           legend('Training Data')
+        end
+        
+        function h = showModel(obj)
+           h = obj.showTrainingData();
+           
+           % ========= YOUR CODE HERE =========
+           hold on;
+           % add the final trained model plot to the figure ('b-')
+           plot(obj.trainingData.feature, obj.hypothesis(), 'b-')
+           % update the legend
+           legend('Training Data', 'Linear Regression Model')
+           hold off;
+           
+        end
+        
+        function setTheta(obj,theta0,theta1)
+            obj.theta = [theta0;theta1];
+        end
+        
+        function setThetaOptimum(obj,theta0,theta1)
+            obj.thetaOptimum = [theta0;theta1];
+        end
+    end
+    
+    methods (Access = private)
+        
+        function initializeTheta(obj)
+            obj.setTheta(0,0);
+        end
+   
+    end
+end
+
+
